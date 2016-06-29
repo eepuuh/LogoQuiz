db51fa4c1c728cc0bb3979caf0b7e05100d30a7f
eepuuh <ssgirlbeibi@gmail.com>
2016-06-29 20:36:53 +0300

    LogoQuiz
    test

---
+++ LogoQuiz.js
@@ -0,0 +1,122 @@
+function katsoOikeat() {
+
+	var inputVal = document.getElementById("iglogo");
+
+	if (inputVal.value === "instagram") {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("fblogo");
+
+	if (inputVal.value === "facebook") {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("mtnlogo");
+
+	if (inputVal.value === "mountain dew") {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("prinlogo");
+
+	if (inputVal.value === "pringles") {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("chupalogo");
+
+	if (inputVal.value === "chupa chups") {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("inlogo");
+
+	if (inputVal.value === "intel" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("adlogo");
+
+	if (inputVal.value === "adidas" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("bglogo");
+
+	if (inputVal.value === "burger king" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("dblogo");
+
+	if (inputVal.value === "dropbox" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("laclogo");
+
+	if (inputVal.value === "lacoste" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("allogo");
+
+	if (inputVal.value === "alko" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+
+	var inputVal = document.getElementById("danlogo");
+
+	if (inputVal.value === "danone" ) {
+		inputVal.style.backgroundColor = "rgba(89, 196, 8, 0.49)";
+		inputVal.style.color = "white";
+	}	else {
+		inputVal.style.backgroundColor = "white";
+		inputVal.style.color = "red";
+	}
+}
